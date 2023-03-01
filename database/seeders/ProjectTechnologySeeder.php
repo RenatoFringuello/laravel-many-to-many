72fabd069ef7f0f81202afc397c71b5019ccb44a
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-01 17:52:13 +0100

    add project-technology seeder

---
+++ database/seeders/ProjectTechnologySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectTechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $projects = Project::all();
+
+        foreach ($projects as $project) {
+            $project->technologies()->attach(Technology::inRandomOrder()->limit(rand(1, 5))->get());
+        }
+    }
+}
