05852ada23304de08ab8e22aefae3c84a1397445
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-01 17:18:38 +0100

    add slug column

--- database/migrations/2023_03_01_125459_create_technologies_table.php
+++ database/migrations/2023_03_01_125459_create_technologies_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
+            $table->string('slug', 100)->unique();
             $table->string('bg_color', 9);
             $table->timestamps();
         });
